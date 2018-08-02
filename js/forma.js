d4c5969f4b05d6f80c1d7305970d3c2470506a8e
Alex-Koltun <aleksandrtr91@gmail.com>
2018-08-02 23:02:38 +0300

    M4T1 and T2
    T1 :
    - call gallery pop-up to click and keydown, with all photos.
    
    T2 :
    - call upload pop-up click and clock, keydown
    - change size photo
    - change filtres

---
+++ js/forma.js
@@ -0,0 +1,76 @@
+'use strict';
+var uploadSelect = document.querySelector('#upload-select-image');
+var uploadFilter = document.querySelector('#upload-filter');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFile = document.querySelector('#upload-file');
+var uploadResizeDec = uploadFilter.querySelector('.upload-resize-controls-button-dec');
+var uploadResizeInc = uploadFilter.querySelector('.upload-resize-controls-button-inc');
+var uploadResizeControls = uploadFilter.querySelector('.upload-resize-controls-value');
+
+uploadSelect.classList.remove('invisible');
+uploadFilter.classList.remove('invisible');
+
+
+function change(evt){
+  uploadOverlay.classList.remove('hidden');
+};
+
+function closeFilter(evt) {
+  if ((evt.type === 'keydown' && evt.keyCode === ESC_KEYCODE)||(evt.type === 'click')) {
+    document.querySelector('.upload-overlay').classList.add('hidden');
+    document.removeEventListener('keydown', closeFilter);
+  }
+  uploadFilter.querySelector('#upload-cancel').addEventListener('keydown', function(evt) {
+      if(evt.type === 'keydown' && evt.keyCode === ENTER_KEYCODE){
+        document.querySelector('.upload-overlay').classList.add('hidden');
+        document.removeEventListener('keydown', closeFilter);
+      };
+    });
+};
+
+var minValueResize = 25;
+var maxValueResize = 100;
+var step = 25;
+
+function resizeInc() {
+  var currentValue = +uploadOverlay.querySelector('.upload-resize-controls-value').getAttribute('value').replace("%","");
+  var resultInc = currentValue + step;
+  var resultString = String (resultInc/100);
+  var x = 'scale(' + resultString +')' ;
+
+if(resultInc <= maxValueResize){
+  var newValue = uploadOverlay.querySelector('.upload-resize-controls-value').setAttribute('value', resultInc + '%');
+  uploadOverlay.querySelector('.upload-form-preview').style.transform = x;
+}
+};
+
+function resizeDec() {
+  var currentValue = +uploadOverlay.querySelector('.upload-resize-controls-value').getAttribute('value').replace("%","");
+  var resultDec = currentValue - step;
+  var resultString = String (resultDec/100);
+  var x = 'scale(' + resultString +')' ;
+
+  if (resultDec >= 25) {
+    var newValue = uploadOverlay.querySelector('.upload-resize-controls-value').setAttribute('value', resultDec + '%');
+    uploadOverlay.querySelector('.upload-form-preview').style.transform = x;
+  };
+};
+
+var effectList = uploadOverlay.querySelectorAll('.upload-filter-controls input');
+
+function addEffect(evt) {
+  var effectImage = uploadOverlay.querySelector('.filter-image-preview');
+  var y = evt.target.id;
+  var effectId = y.replace("upload-", "");
+
+  effectImage.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+  effectImage.classList.add(effectId);
+};
+
+for( var i = 0 ; i < effectList.length; i++) {
+  effectList[i].addEventListener('click', addEffect);
+};
+
+uploadFile.addEventListener('change', change);
+document.addEventListener('keydown', closeFilter);
+uploadOverlay.querySelector('#upload-cancel').addEventListener('click',closeFilter);
--- js/pictures.js
+++ js/pictures.js
@@ -5,41 +5,113 @@ var COMMENTS = [ 'Всё отлично!', 'В целом всё неплохо.
 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var urlWay = ['photos/', '.jpg'];
+var likesSummary = generateRandomNumber(15, 200);
 var pictureTemplate = document.querySelector('#picture-template').content;
 var picturesBlock = document.querySelector('.pictures');
-var galleryOverlay = document.querySelector('.gallery-overlay .gallery-overlay-preview');
-var likesSummary = generateRandomNumber(15, 200);
+var galleryOverlayPreview = document.querySelector('.gallery-overlay .gallery-overlay-preview');
+var galleryOverlay = document.querySelector('.gallery-overlay');
 var photoMax = 24;
-var propertiesOtherUsers = [];
+var pictures = [];
+var ipictures = generateRandomNumber(0, pictures.length - 1);
+var galleryClose = galleryOverlay.querySelector('.gallery-overlay-close');
+
 
 function generateRandomNumber(min, max) {
   return Math.floor((Math.random() * (max - min) + 1) + min);
 };
 
+function closeGallery(evt){
+      if ((evt.type === 'keydown' && evt.keyCode === ESC_KEYCODE)||(evt.type === 'click')) {
+        galleryOverlay.classList.add('invisible');
+        document.querySelector('.upload-overlay').classList.add('hidden');
+        document.removeEventListener('keydown', closeGallery);
+      }
+      galleryClose.addEventListener('keydown', function(evt) {
+          if(evt.type === 'keydown' && evt.keyCode === ENTER_KEYCODE){
+            galleryOverlay.classList.add('invisible');
+            document.removeEventListener('keydown', closeGallery);
+          };
+        });
+  };
+
+
 for (var i = 0; i <= photoMax; i++) {
-  propertiesOtherUsers.push({
+  pictures.push({
     url: urlWay[0] + (i + 1) + urlWay[1],
     likes: generateRandomNumber(15, 200),
     comments: COMMENTS[generateRandomNumber(0, COMMENTS.length - 1)]
+  })
+};
+
+// pictureTemplate.querySelector('.picture img ').hasAttribute('src');
+
+for (var j = 0; j < pictures.length; j++) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture-stats .picture-likes').textContent = pictures[j].likes;
+  pictureElement.querySelector('.picture-stats .picture-comments').textContent = pictures[j].comments;
+  pictureElement.querySelector('.picture img').setAttribute('src', pictures[j].url);
+  pictureElement.querySelector('.picture img').setAttribute('src', pictures[j].url);
+  pictureElement.querySelector('.picture img').setAttribute('tabindex', 0);
+
+  picturesBlock.appendChild(pictureElement);
+
+};
+
+var pictureList = picturesBlock.querySelectorAll('.picture');
+
+for( var k = 0; k < pictureList.length; k++) {
+  pictureList[k].addEventListener('click', openGallery);
+  pictureList[k].addEventListener('keydown', openGallery);
+  pictureList[k].addEventListener('keydown',function(evt){
+    evt.preventDefault();
   });
 };
 
-pictureTemplate.querySelector('.picture img ').hasAttribute('src');
+function openGallery(evt) {
+  var imageUrl = evt.target.attributes[0].textContent;
 
-for (var j = 0; j < propertiesOtherUsers.length; j++) {
+  evt.preventDefault();
 
-  var pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture-stats .picture-likes').textContent = propertiesOtherUsers[j].likes;
-  pictureElement.querySelector('.picture-stats .picture-comments').textContent = propertiesOtherUsers[j].comments;
-  pictureElement.querySelector('.picture img').setAttribute('src', propertiesOtherUsers[j].url);
+  if ((evt.type === 'keydown' && evt.keyCode === ENTER_KEYCODE) || (evt.type === 'click')) {
+    galleryOverlay.classList.remove('invisible');
+  };
+  if (evt.type === 'keydown' &&  evt.keyCode === ENTER_KEYCODE){
+      imageUrl = evt.target.children[0].attributes[0].textContent;
+    };
 
-  picturesBlock.appendChild(pictureElement);
+    document.addEventListener('keydown', closeGallery);
+
+  var imageProperties = pictures.find(function(item) {
+    if(item.url === imageUrl) {
+      return item;
+    }
+  });
+
+  galleryOverlayPreview.querySelector('.gallery-overlay-controls .likes-count').textContent = imageProperties.likes;
+  galleryOverlayPreview.querySelector('.gallery-overlay-controls .comments-count').textContent = imageProperties.comments;
+
+  galleryOverlayPreview.querySelector('img').setAttribute('src', imageUrl);
+  // if (evt.type === 'keydown' && evt.keyCode === ENTER_KEYCODE) {
+  // galleryOverlayPreview.querySelector('img').setAttribute('src', keydownImageUrl);
+  // };
 };
-var ipropertiesOtherUsers = generateRandomNumber(0, propertiesOtherUsers.length);
 
-galleryOverlay.querySelector('img').setAttribute('src', propertiesOtherUsers[ipropertiesOtherUsers].url);
-galleryOverlay.querySelector('.gallery-overlay-controls .likes-count').textContent = propertiesOtherUsers[ipropertiesOtherUsers].likes;
-galleryOverlay.querySelector('.gallery-overlay-controls .gallery-overlay-controls-comments').textContent = propertiesOtherUsers[ipropertiesOtherUsers].comments;
+// document.querySelector('.pictures .picture').addEventListener('click', openGallery);
+
 document.querySelector('.upload-overlay').classList.add('hidden');
-document.querySelector('.gallery-overlay').classList.remove('invisible');
+
+galleryClose.addEventListener('click', closeGallery);
+galleryClose.addEventListener('keydown', closeGallery);
+
+
+
+// galleryOverlay.classList.remove('invisible');
+
+
+
+// document.addEventListener('keydown', closeGallery);
+// picturesBlock.addEventListener('mouseover', photo);
