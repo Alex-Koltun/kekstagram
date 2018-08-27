3dbc1f49d286b008a26012d484d4fde425875c89
Alex-Koltun <aleksandrtr91@gmail.com>
2018-08-27 15:52:43 +0300

    M5-t2
    Move pipka
    Change filter contrast.

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@
 (function(){
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
+  var INIT_CONST = 20 + '%';
   var uploadSelect = document.querySelector('#upload-select-image');
   var uploadFilter = document.querySelector('#upload-filter');
   var uploadOverlay = document.querySelector('.upload-overlay');
@@ -9,9 +10,15 @@
   var uploadResizeDec = uploadFilter.querySelector('.upload-resize-controls-button-dec');
   var uploadResizeInc = uploadFilter.querySelector('.upload-resize-controls-button-inc');
   var uploadResizeControls = uploadFilter.querySelector('.upload-resize-controls-value');
+  var pipka = uploadFilter.querySelector('.upload-filter-level-pin');
+  var linePipka = uploadFilter.querySelector('.upload-filter-level-line');
+  var pipkaCont = uploadFilter.querySelector('.upload-filter-level');
+  var lineVal = uploadFilter.querySelector('.upload-filter-level-val');
+  var filterContrast = document.querySelector(".filter-image-preview");
 
   uploadSelect.classList.remove('invisible');
   uploadFilter.classList.remove('invisible');
+  pipkaCont.classList.add('invisible');
 
 
   function change(evt){
@@ -64,19 +71,105 @@
   function addEffect(evt) {
     var effectImage = uploadOverlay.querySelector('.filter-image-preview');
     var y = evt.target.id;
-    var effectId = y.replace("upload-", "");
+    window.effectId = y.replace("upload-", "");
 
     effectImage.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
     effectImage.classList.add(effectId);
+    pipkaCont.classList.remove('invisible');
+    pipka.style.left = INIT_CONST;
+    lineVal.style.width = INIT_CONST;
+    if(window.effectId === "filter-none") {
+      pipkaCont.classList.add('invisible');
+    }
+    if(window.effectId === "filter-chrome") {
+      filterContrast.style.cssText = 'filter : grayscale('+ (INIT_CONST.replace("px","")) +')';
+    }
+    if(window.effectId === "filter-sepia") {
+      filterContrast.style.cssText = 'filter : sepia('+ (INIT_CONST.replace("px","")) +')';
+    }
+    if(window.effectId === "filter-marvin") {
+      filterContrast.style.cssText = 'filter : invert('+ (INIT_CONST.replace("px","")) +'%)';
+    }
+    if(window.effectId === "filter-phobos") {
+      filterContrast.style.cssText = 'filter : blur('+ (INIT_CONST.replace("px","")*(3/100)) +'px)';
+    }
+    if(window.effectId === "filter-heat") {
+      filterContrast.style.cssText = 'filter : brightness('+ (INIT_CONST.replace("px","")*(3/100)) +')';
+    }
   };
 
   for( var i = 0 ; i < effectList.length; i++) {
     effectList[i].addEventListener('click', addEffect);
   };
 
+
+  function activatePipka(evt) {
+    evt.preventDefault();
+
+    var initX = evt.offsetX;
+    var pipkaX = +getComputedStyle(pipka).left.replace("px","");
+    var linePipkaLong = getComputedStyle(linePipka).width.replace("px","");
+
+    function movePipka(event) {
+        event.preventDefault();
+
+        var coordX = event.offsetX;
+        var deltaX = coordX - initX;
+        var changeX = pipkaX + deltaX;
+        window.changeMove = Math.round((changeX * 100)/linePipkaLong);
+        window.roundChange = changeMove + '%';
+
+        if (changeMove <= 100 || changeMove <= 0) {
+          pipka.style.left = roundChange;
+          lineVal.style.width = roundChange;
+        };
+
+        if(window.effectId === "filter-chrome") {
+          // filterContrast.style.cssText = 'filter : grayscale('+ (INIT_CONST.replace("px","")) +')';
+          filterContrast.style.cssText = 'filter : grayscale('+ (window.changeMove/100) +')';
+        };
+
+        if(window.effectId === "filter-sepia") {
+          // filterContrast.style.cssText = 'filter : sepia('+ (INIT_CONST.replace("px","")) +')';
+          filterContrast.style.cssText = 'filter : sepia('+ (window.changeMove/100) +')';
+        };
+
+        if(window.effectId === "filter-marvin") {
+          // filterContrast.style.cssText = 'filter : invert('+ (INIT_CONST.replace("px","")) +')';
+          filterContrast.style.cssText = 'filter : invert('+ window.roundChange + ')';
+        };
+
+        if(window.effectId === "filter-phobos") {
+          filterContrast.style.cssText = 'filter : blur('+ (INIT_CONST.replace("px","")) +')';
+          filterContrast.style.cssText = 'filter : blur('+ window.changeMove * (3/100) +'px)';
+        };
+
+        if(window.effectId === "filter-heat") {
+          filterContrast.style.cssText = 'filter : brightness('+ (INIT_CONST.replace("px","")) +')';
+          filterContrast.style.cssText = 'filter : brightness('+ window.changeMove * (3/100) +')';
+        };
+
+      };
+
+     function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', movePipka);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', movePipka);
+    document.addEventListener('mouseup', onMouseUp);
+
+    linePipka.addEventListener('dragover', function (evt) {
+      evt.preventDefault();
+      return false;
+    });
+  };
+
+  pipkaCont.addEventListener('mousedown', activatePipka);
+  uploadResizeInc.addEventListener('click', resizeInc);
+  uploadResizeDec.addEventListener('click', resizeDec);
   uploadFile.addEventListener('change', change);
   document.addEventListener('keydown', closeFilter);
   uploadOverlay.querySelector('#upload-cancel').addEventListener('click',closeFilter);
-
-
 })();
